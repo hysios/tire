0adc4f061ae3907606b6f7acb254eaa5a260742f
hysios <hysios@gmail.com>
2013-08-09 15:38:14 +0800

    修复 options 拼写 bug

--- lib/tire/search/sort.rb
+++ lib/tire/search/sort.rb
@@ -5,7 +5,7 @@ module Tire
       def initialize(*args, &block)
         @value = []
         args.each do |options|
-          @value << options if optinos.is_a?(Hash)
+          @value << options if options.is_a?(Hash)
         end
         block.arity < 1 ? self.instance_eval(&block) : block.call(self) if block_given?
       end
